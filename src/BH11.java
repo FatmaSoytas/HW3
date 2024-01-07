54a181468d2ca1ee3e306574fb6bc59536d2c7ac
Fatma SOYTAS <fatmasoytas81@homail.com>
2024-01-07 19:46:21 +0100

    BH11 dosyasi eklendi

--- src/BH11.java
+++ src/BH11.java
@@ -2,7 +2,6 @@ public class BH11 {
     public static void main(String[] args) {
         System.out.println(" busefer olacak !!!");
         //Yeni satir
-        
            System.out.println(" Bravo bu sefer oldu :) ");
     }
 }
