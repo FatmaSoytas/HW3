294ee6836dcdd840612f4aa930f58973bacfa142
Fatma SOYTAS <fatmasoytas81@homail.com>
2024-01-07 17:28:58 +0100

    Test1 ve Test2 yeni testler eklendi

--- src/Test1.java
+++ src/Test1.java
@@ -1,5 +1,6 @@
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("2. ev ödevini yapiyoruz");
+        System.out.println(" ödevler guzel gidiyor");
     }
 }
--- src/Test2.java
+++ src/Test2.java
@@ -1,5 +1,6 @@
 public class Test2 {
     public static void main(String[] args) {
         System.out.println(" Bugun gunlerden Pazar");
+        System.out.println(" Yarin Pazartesi");
     }
 }
